a597fa1b682567266108974aa8b2a2ce5675b7f5
Vio IT <150695785+vioit22@users.noreply.github.com>
2024-09-16 00:18:48 +0300

    Add files via upload

---
+++ Hash Code for all Numbers/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+internal partial class Program {
+    private static void Main() {
+        
+// For number 1
+int one = 1;
+double one1 = Convert.ToDouble(one);
+Console.WriteLine("For number 1, Hash Code is:");
+Console.WriteLine(one1.GetHashCode());
+// For number 2
+int two = 2;
+double two2 = Convert.ToDouble(two);
+Console.WriteLine("For number 2, Hash Code is:");
+Console.WriteLine(two2.GetHashCode());
+// For number 3
+int three = 3;
+double three3 = Convert.ToDouble(three);
+Console.WriteLine("For number 3, Hash Code is:");
+Console.WriteLine(three3.GetHashCode());
+// For number 4
+int four = 4;
+double four4 = Convert.ToDouble(four);
+Console.WriteLine("For number 4, Hash Code is:");
+Console.WriteLine(four4.GetHashCode());
+// For number 5
+int five = 5;
+double five5 = Convert.ToDouble(five);
+Console.WriteLine("For number 5, Hash Code is:");
+Console.WriteLine(five5.GetHashCode());
+// For number 6
+int six = 6;
+double six6 = Convert.ToDouble(six);
+Console.WriteLine("For number 6, Hash Code is:");
+Console.WriteLine(six6.GetHashCode());
+// For number 7
+int seven = 7;
+double seven7 = Convert.ToDouble(seven);
+Console.WriteLine("For number 7, Hash Code is:");
+Console.WriteLine(seven7.GetHashCode());
+// For number 8
+int eight = 8;
+double eight8 = Convert.ToDouble(eight);
+Console.WriteLine("For the number 8, Hash Code is:");
+Console.WriteLine(eight8.GetHashCode());
+// For number 9
+int nine = 9;
+double nine9 = Convert.ToDouble(nine);
+Console.WriteLine("For the number 9, Hash Code is:");
+Console.WriteLine(nine9.GetHashCode());
+// For number 10
+int ten = 10;
+double ten10 = Convert.ToDouble(ten);
+Console.WriteLine("For the number 10, Hash Code is:");
+Console.WriteLine(ten10.GetHashCode());
+// For number 11
+int eleven = 11;
+double eleven11 = Convert.ToDouble(eleven);
+Console.WriteLine("For the number 11, Hash Code is:");
+Console.WriteLine(eleven11.GetHashCode());
+Console.WriteLine("Press Enter to exit... Developed by Vio IT!");
+Console.ReadLine();
+    }
+}

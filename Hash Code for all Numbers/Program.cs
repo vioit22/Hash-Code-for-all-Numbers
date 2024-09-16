549df5f2eeedb86dd7cf9c4d63d1fc38802b51b0
Vio IT <150695785+vioit22@users.noreply.github.com>
2024-09-16 13:00:46 +0300

    Add files via upload

--- Hash Code for all Numbers/Program.cs
+++ Hash Code for all Numbers/Program.cs
@@ -57,6 +57,101 @@ int eleven = 11;
 double eleven11 = Convert.ToDouble(eleven);
 Console.WriteLine("For the number 11, Hash Code is:");
 Console.WriteLine(eleven11.GetHashCode());
+// For number 12
+int twelve = 12;
+double twelve12 = Convert.ToDouble(twelve);
+Console.WriteLine("For the number 12, Hash Code is:");
+Console.WriteLine(twelve12.GetHashCode());
+// For number 13
+int thirty = 13;
+double thirty13 = Convert.ToDouble(thirty);
+Console.WriteLine("For the number 13, Hash Code is:");
+Console.WriteLine(thirty13.GetHashCode());
+// For number 14
+int number14 = 14;
+double number1414 = Convert.ToDouble(number14);
+Console.WriteLine("For the number 14, Hash Code is:");
+Console.WriteLine(number1414.GetHashCode());
+// For number 15
+int number15 = 15;
+double number1515 = Convert.ToDouble(number15);
+Console.WriteLine("For the number 15, Hash Code is:");
+Console.WriteLine(number1515.GetHashCode());
+// For number 16
+int number16 = 16;
+double number1616 = Convert.ToDouble(number16);
+Console.WriteLine("For the number 16, Hash Code is:");
+Console.WriteLine(number1616.GetHashCode());
+// For number 17
+int number17 = 17;
+double number1717 = Convert.ToDouble(number17);
+Console.WriteLine("For the number 17, Hash Code is:");
+Console.WriteLine(number1717.GetHashCode());
+// For number 18
+int number18 = 18;
+double number1818 = Convert.ToDouble(number18);
+Console.WriteLine("For the number 18, Hash Code is:");
+Console.WriteLine(number1818.GetHashCode());
+// For number 19
+int number19 = 19;
+double number1919 = Convert.ToDouble(number19);
+Console.WriteLine("For the number 19, Hash Code is:");
+Console.WriteLine(number1919.GetHashCode());
+// For number 20
+int number20 = 20;
+double number2020 = Convert.ToDouble(number20);
+Console.WriteLine("For the number 20, Hash Code is:");
+Console.WriteLine(number2020.GetHashCode());
+// For number 21
+int number21 = 21;
+double number2121 = Convert.ToDouble(number21);
+Console.WriteLine("For the number 21, Hash Code is:");
+Console.WriteLine(number2121.GetHashCode());
+// For number 22
+int number22 = 22;
+double number2222 = Convert.ToDouble(number22);
+Console.WriteLine("For the number 22, Hash Code is:");
+Console.WriteLine(number2222.GetHashCode());
+// For number 23
+int number23 = 23;
+double number2323 = Convert.ToDouble(number23);
+Console.WriteLine("For the number 23, Hash Code is:");
+Console.WriteLine(number2323.GetHashCode());
+// For number 24
+int number24 = 24;
+double number2424 = Convert.ToDouble(number24);
+Console.WriteLine("For the number 24, Hash Code is:");
+Console.WriteLine(number2424.GetHashCode());
+// For number 25
+int number25 = 25;
+double number2525 = Convert.ToDouble(number25);
+Console.WriteLine("For the number 25, Hash Code is:");
+Console.WriteLine(number2525.GetHashCode());
+// For number 26
+int number26 = 26;
+double number2626 = Convert.ToDouble(number26);
+Console.WriteLine("For the number 26, Hash Code is:");
+Console.WriteLine(number2626.GetHashCode());
+// For number 27
+int number27 = 27;
+double number2727 = Convert.ToDouble(number27);
+Console.WriteLine("For the number 27, Hash Code is:");
+Console.WriteLine(number2727.GetHashCode());
+// For number 28
+int number28 = 28;
+double number2828 = Convert.ToDouble(number28);
+Console.WriteLine("For the number 28, Hash Code is:");
+Console.WriteLine(number2828.GetHashCode());
+// For number 29
+int number29 = 29;
+double number2929 = Convert.ToDouble(number29);
+Console.WriteLine("For the number 29, Hash Code is:");
+Console.WriteLine(number2929.GetHashCode());
+// For number 30
+int number30 = 30;
+double number3030 = Convert.ToDouble(number30);
+Console.WriteLine("For the number 30, Hash Code is:");
+Console.WriteLine(number3030.GetHashCode());
 Console.WriteLine("Press Enter to exit... Developed by Vio IT!");
 Console.ReadLine();
     }
